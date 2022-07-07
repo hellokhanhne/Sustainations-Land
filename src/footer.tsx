527ee63f1d52ed693d20266247aaf982601590c2
Nguyen Ngoc Khanh <84921391+hellokhanhne@users.noreply.github.com>
2022-07-07 17:21:33 +0700

    Initial commit

---
+++ src/footer.tsx
@@ -0,0 +1,88 @@
+import * as React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        padding: "10px 20px",
+        backgroundColor: "rgba(0,0,0,.6)",
+        bottom: "15px",
+
+        zIndex: 10000,
+        borderTopRightRadius: 5,
+        borderBottomRightRadius: 5,
+      }}
+    >
+      <div className="d-flex">
+        <div
+          className="d-flex align-items-center"
+          style={{
+            marginRight: 25,
+          }}
+        >
+          <div
+            className="item"
+            style={{
+              backgroundColor: "#002E5E",
+            }}
+          ></div>
+          <span
+            style={{
+              color: "#fff",
+              marginLeft: 10,
+            }}
+          >
+            Available
+          </span>
+        </div>
+
+        <div
+          className="d-flex align-items-center"
+          style={{
+            marginRight: 25,
+          }}
+        >
+          <div
+            className="item"
+            style={{
+              backgroundColor: "#48c3c8",
+            }}
+          ></div>
+          <span
+            style={{
+              color: "#fff",
+              marginLeft: 10,
+            }}
+          >
+            Purchased
+          </span>
+        </div>
+
+        <div
+          className="d-flex align-items-center"
+          style={{
+            marginRight: 25,
+          }}
+        >
+          <div
+            className="item"
+            style={{
+              backgroundColor: "#FAA61A",
+            }}
+          ></div>
+          <span
+            style={{
+              color: "#fff",
+              marginLeft: 10,
+            }}
+          >
+            Premium
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
